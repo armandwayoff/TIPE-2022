e2484f13794188761aab51115e9fc88e66a82b44
Armand Wayoff <armandwayoff@lavabit.com>
2021-10-30 20:47:41 +0200

    Update main.py

--- GUI/main.py
+++ GUI/main.py
@@ -4,9 +4,9 @@ from tkinter import *
 
 FONT_FAMILY = "Helvetica"
 TITLE = "Interface de commande du robot"
-TITLE_PORTS_WINDOW = "Sélection du port série"
-FONT_SIZE_TITLE = 18
-FONT_SIZE = 12
+TITLE_PORTS_WINDOW = "Sélectionner un port série"
+FONT_SIZE_TITLE = 12
+FONT_SIZE = 10
 FONT_SIZE_AUTHOR = 8
 
 # Fenêtre principale
@@ -24,12 +24,15 @@ def open_ports_window():
     serial_obj = serial.Serial()
 
     def init_com_port(index):
+        global port_state
         current_port = str(ports[index])
         com_port_var = str(current_port.split(' ')[0])
         serial_obj.port = com_port_var
         serial_obj.baudrate = 9600
         if not serial_obj.isOpen():
             serial_obj.open()
+        label_select_port.config(text="Port série sélectionné : " + com_port_var)
+        ports_window.destroy()
 
     if len(ports) > 0:
         for onePort in ports:
@@ -37,19 +40,29 @@ def open_ports_window():
                                 command=functools.partial(init_com_port, index=ports.index(onePort)))
             com_button.grid(row=ports.index(onePort) + 1, column=0, padx=10, pady=10)
     else:
-        lb1 = Label(ports_window,
-                    text="Aucun port série disponible",
-                    font=(FONT_FAMILY, FONT_SIZE))
-        lb1.grid(row=1, column=0)
+        no_ports = Label(ports_window,
+                         text="Aucun port série disponible",
+                         font=(FONT_FAMILY, FONT_SIZE))
+        no_ports.grid(row=1, column=0, padx=10, pady=10)
 
 
-btn = Button(root, text=TITLE_PORTS_WINDOW, command=open_ports_window)
-btn.grid(row=1, column=0, columnspan=2, padx=10, pady=10)
+label_select_port = Label(root,
+                          text="Port série sélectionné : ",
+                          font=(FONT_FAMILY, FONT_SIZE))
+label_select_port.grid(row=1, column=0, padx=10, pady=10)
 
-title = Label(root,
-              text=TITLE,
-              font=(FONT_FAMILY, FONT_SIZE_TITLE))
-title.grid(row=0, column=0, columnspan=2, padx=20, pady=10)
+btn = Button(root,
+             text=TITLE_PORTS_WINDOW,
+             command=open_ports_window)
+btn.grid(row=1, column=1, padx=10, pady=10)
+
+main_title = Label(root,
+                   text=TITLE,
+                   font=(FONT_FAMILY, FONT_SIZE_TITLE),
+                   bg="white",
+                   borderwidth=1,
+                   relief="solid")
+main_title.grid(row=0, column=0, columnspan=2, padx=20, pady=10, ipadx=10, ipady=10)
 
 author = Label(root,
                text="Armand Wayoff - 2021",
@@ -57,25 +70,25 @@ author = Label(root,
 author.grid(row=15, column=0, columnspan=2, pady=10)
 
 label_scale_air = Label(root,
-                         text="Slider air",
-                         font=(FONT_FAMILY, FONT_SIZE))
+                        text="Slider air",
+                        font=(FONT_FAMILY, FONT_SIZE))
 label_scale_air.grid(row=2, column=0)
 
 scale_air = Scale(root,
-                   from_=0,
-                   to=100,
-                   orient=HORIZONTAL)
+                  from_=0,
+                  to=100,
+                  orient=HORIZONTAL)
 scale_air.grid(row=2, column=1)
 
 label_scale_camera = Label(root,
-                            text="Slider camera",
-                            font=(FONT_FAMILY, FONT_SIZE))
+                           text="Slider camera",
+                           font=(FONT_FAMILY, FONT_SIZE))
 label_scale_camera.grid(row=3, column=0)
 
 scale_camera = Scale(root,
-                      from_=0,
-                      to=100,
-                      orient=HORIZONTAL)
+                     from_=0,
+                     to=100,
+                     orient=HORIZONTAL)
 scale_camera.grid(row=3, column=1)
 
 root.mainloop()
