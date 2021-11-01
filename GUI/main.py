5226c69433dfe4ada403365c75503877e0c43ebd
Armand Wayoff <armandwayoff@lavabit.com>
2021-11-01 16:26:19 +0100

    Update main.py

--- GUI/main.py
+++ GUI/main.py
@@ -11,17 +11,18 @@ TITLE = "Interface de commande du robot"
 FONT_SIZE_TITLE = 12
 FONT_SIZE = 10
 FONT_SIZE_AUTHOR = 8
-DELAY = 500  # ms
-
-state_port = False
+DELAY = 50  # ms
 
 # Main window
 root = Tk()
 root.title(TITLE)
-root.geometry("370x230")
-root.resizable(False, False)
+# root.geometry("370x230")
+# root.resizable(False, False)
 
 ports = serial.tools.list_ports.comports()
+var = IntVar()
+var.set(1)
+state_port = False
 
 
 def open_ports_window():
@@ -66,20 +67,13 @@ def send_data(serial_port):
     root.after(DELAY, send_data)
 
 
-if state_port:
-    root.after(DELAY, send_data)
+def auto_mode():
+    if var.get() == 1:
+        scale_camera.set(scale_air.get() / 2)
+    root.after(DELAY, auto_mode)
+
 
 # Widgets
-
-label_select_port = Label(root,
-                          text="Auncun port série sélectionné",
-                          font=(FONT_FAMILY, FONT_SIZE))
-label_select_port.grid(row=1, column=0, padx=10, pady=10)
-
-button_select_port = Button(root,
-                            text="Sélectionner un port série",
-                            command=open_ports_window)
-button_select_port.grid(row=1, column=1, padx=10, pady=10)
 
 main_title = Label(root,
                    text=TITLE,
@@ -87,34 +81,61 @@ main_title = Label(root,
                    bg="white",
                    borderwidth=1,
                    relief="solid")
-main_title.grid(row=0, column=0, columnspan=2, padx=20, pady=10, ipadx=10, ipady=10)
 
-label_scale_air = Label(root,
-                        text="Débit d'air (%)",
-                        font=(FONT_FAMILY, FONT_SIZE))
-label_scale_air.grid(row=2, column=0)
+label_select_port = Label(root,
+                          text="Auncun port série sélectionné",
+                          font=(FONT_FAMILY, FONT_SIZE))
+button_select_port = Button(root,
+                            text="Sélectionner un port série",
+                            command=open_ports_window)
+
+rb_auto = Radiobutton(root,
+                      text="Mode automatique",
+                      variable=var,
+                      value=1)
+rb_manuel = Radiobutton(root,
+                        text="Mode manuel",
+                        variable=var,
+                        value=2)
 
 scale_air = Scale(root,
                   from_=0,
                   to=100,
-                  orient=HORIZONTAL)
-scale_air.grid(row=2, column=1)
-
-label_scale_camera = Label(root,
-                           text="Vitesse du cable (%)",
-                           font=(FONT_FAMILY, FONT_SIZE))
-label_scale_camera.grid(row=3, column=0)
-
+                  orient=HORIZONTAL,
+                  label="Débit d'air (%)",
+                  length=120)
 scale_camera = Scale(root,
                      from_=0,
                      to=100,
-                     orient=HORIZONTAL)
-scale_camera.grid(row=3, column=1)
+                     orient=HORIZONTAL,
+                     label="Vitesse du cable (%)",
+                     length=120)
 
 author = Label(root,
                text="Armand Wayoff - 2021",
                font=(FONT_FAMILY, FONT_SIZE_AUTHOR),
                fg="grey")
+
+# Positions
+
+main_title.grid(row=0, column=0, columnspan=2, padx=20, pady=10, ipadx=10, ipady=10)
+
+button_select_port.grid(row=1, column=1, padx=10, pady=10)
+label_select_port.grid(row=1, column=0, padx=10, pady=10)
+
+rb_auto.grid(row=2, column=0)
+rb_manuel.grid(row=3, column=0)
+
+scale_air.grid(row=2, column=1)
+scale_camera.grid(row=3, column=1)
+
 author.grid(row=15, column=0, columnspan=2, pady=10)
+
+# Functions
+
+if state_port:
+    root.after(DELAY, send_data)
+
+root.after(DELAY, auto_mode)
 
 root.mainloop()
